fd2e6fcb51da759b0fb161120f086d9cb092394c
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2019-05-28 22:55:00 -0400

    added terms for tempo changes

--- cmds/terms.js
+++ cmds/terms.js
@@ -22,15 +22,41 @@ Prestissimo       >200     extremely fast
 
 Additional Terms:
 
-A piacere                            the performer may use his own discretion with
-                                     regard to tempo and rhythm; literally "at pleasure"
-L'istesso tempo or Lo stesso tempo   at the same speed
-Tempo comodo                         at a comfortable (normal) speed
-Tempo di...                          the speed of a ...
-                                     - Tempo di valse (speed of a waltz)
-                                     - Tempo di marcia (speed of a march)
-Tempo giusto                         at a consistent speed, at the 'right' speed, in strict tempo
-Tempo semplice                       simple, regular speed, plainly
+A piacere                           the performer may use his own discretion with
+                                    regard to tempo and rhythm; literally "at pleasure"
+L'istesso tempo or Lo stesso tempo  at the same speed
+Tempo comodo                        at a comfortable (normal) speed
+Tempo di...                         the speed of a ...
+                                    - Tempo di valse (speed of a waltz)
+                                    - Tempo di marcia (speed of a march)
+Tempo giusto                        at a consistent speed, at the 'right' speed, in strict tempo
+Tempo semplice                      simple, regular speed, plainly
+
+Terms For Change In Tempo:
+
+Accelerando       speeding up (abbreviation: accel.)
+Allargando        growing broader; decreasing tempo, usually near the end of a piece
+Calando           going slower (and usually also softer)
+Doppio movimento  double speed
+Lentando          gradual slowing and softer
+Meno mosso        less movement or slower
+Mosso             movement, more lively, or quicker, much like più mosso, but not as extreme
+Più mosso         more movement or faster
+Precipitando      hurrying, going faster/forward
+Rallentando       gradual slowing down (abbreviation: rall.)
+Ritardando        less gradual slowing down (more sudden decrease in tempo than rallentando;
+                  abbreviation: rit. or more specifically, ritard.)
+Ritenuto          slightly slower; temporarily holding back.
+                  (Note that the abbreviation for ritenuto can also be rit.
+                  Thus a more specific abbreviation is riten. Also, sometimes, ritenuto
+                  does not reflect a tempo change but a character change instead.)
+Rubato            free adjustment of tempo for expressive purposes
+Stretto           in faster tempo, often near the conclusion of a section.
+                  (Note that in fugal compositions, the term stretto refers to the
+                  imitation of the subject in close succession, before the subject is
+                  completed, and as such, suitable for the close of the fugue. Used in
+                  this context, the term is not necessarily related to tempo.)
+Stringendo        pressing on faster (literally "tightening")
 `
   console.log(terms)
 }
